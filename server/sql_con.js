86b623d208471210668b257ce95d22d7a9c66851
unknown <debapriya.susmoy1999@gmail.com>
2022-08-09 16:33:09 +0600

    done

--- server/sql_con.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql';
-var dbconfig={
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "project",
-    multipleStatements: true
-  }
-export var   con=mysql.createConnection(dbconfig);
